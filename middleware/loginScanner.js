8f28e68cac6c52b4585f99dd973bc9963a1d21a9
Bhaesh yadav <ydvbhavesh5@gmail.com>
2026-03-01 21:05:36 +0545

    Backend Update 2

--- middleware/loginScanner.js
+++ middleware/loginScanner.js
@@ -1,12 +1,17 @@
 
-import rateLimit from "express-rate-limit";
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 
 export const loginScanner = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
   max: 5,
-  keyGenerator: (req) => req.body.email || req.ip , 
+  keyGenerator: (req) => {
+    if (req.body?.email) {
+      return req.body.email.toLowerCase(); // email based limit
+    }
+    return ipKeyGenerator(req); // safe IPv6 handling
+  },
   message: {
     success: false,
-    message: "Too many login attempts for this account"
-  }
+    message: "Too many login attempts for this account",
+  },
 });
