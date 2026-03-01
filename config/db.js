67bbd761141133a60d258c4440e93a5083b3d8d4
Bhaesh yadav <ydvbhavesh5@gmail.com>
2026-03-01 21:39:55 +0545

    Backend DB Update

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.atlas_URI);
         console.log("Mongoose connected ")
     } catch (error) {
         console.log("mongodb" , error)
